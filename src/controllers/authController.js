a6764171e08935a3181d60fb6aa2f5be1e5b2ac2
breno <breno.beierstedt@hotmail.com>
2019-01-09 05:34:13 -0200

    stage 1 user authentication token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,13 +4,45 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+const Jwt = require('jsonwebtoken');
 
-router.post('/login', async (req,res)=> {
+router.post('/login/:username/:password', async (req,res)=> {
 
-    req.params.user
+    const username = req.params.username;
+    const password = req.params.password;
 
 
-});
+   var query= User.findOne({username, password});
+
+     query.select({username, password});
+
+
+    query.exec(function (err, doc) {
+        if (err) return handleError(err);
+
+        if(!doc){
+            res.status(404).send('usuário não encontrado');
+        }else {
+
+            if ((doc.username === username) && (doc.password === password)) {
+               Jwt.sign({username, password}, 'secret',(err,token)=>{
+                   res.json({
+                       token
+                   })
+               })
+            }
+
+        }
+
+
+    })
+     
+    });
+
+
+
+
+
 
 
 
