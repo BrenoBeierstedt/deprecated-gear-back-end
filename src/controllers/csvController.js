8d6d1b985df850d45003fcb90d3540d78fcaee9f
breno <breno.beierstedt@hotmail.com>
2019-01-12 22:09:19 -0200

    stage two, conclusion of API implementation

--- src/controllers/csvController.js
+++ src/controllers/csvController.js
@@ -1,117 +1,185 @@
+
+
 const express = require('express');
+const app = express();
 
 const Csv = require('../models/commonService');
 
 
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
+
 //create
 
-router.post('/common', async (req,res)=> {
+router.post('/common',verifyToken, async (req,res)=> {
 
-    const csv = new Csv(req.body);
+    jwt.verify(req.token, 'secret',(err,authData)=> {
+        if (err) {
+            res.status(403).json('Authorization not found');
+            console.log('Authorization not found');
 
-    csv
-        .save()
-        .then(result => {
-            console.log(result);
+        } else {
+            const csv = new Csv(req.body);
+
+            csv
+                .save()
+                .then(result => {
+                    console.log(result);
 
 
-        })
-        .catch(err=>{
-            return res.status(500);
-            console.log("Problem creating new document")
-        });
-    res.status(201).json({
-        message: "Succefully created",
-        createdCommon: csv
-    })
+                })
+                .catch(err => {
+                    console.log("Problem creating new document");
+                    return res.status(500);
+
+                });
+            res.status(201).json({
+                message: "Succefully created",
+                createdCommon: csv
+            })
+        }
+    });
 });
 
 //alter
 
-router.put('/common/:id',async (req, res)=>{
-    Csv.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new:true},
-        (err, csv)=>{
-            if(err)return res.status(500).send(err);
-            const response = {
-                message: "Succefully updated",
-                updatedtedCommon: csv
-            };
-            console.log("Alter on C300CSV ID:", req.params.id);
-            return res.status(200).send(response);
+router.put('/common/:id',verifyToken,async (req, res)=> {
 
+    jwt.verify(req.token, 'secret', (err, authData) => {
+        if (err) {
+            res.status(403).json('Authorization not found');
+            console.log('Authorization not found');
+        } else {
+
+            Csv.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                {new: true},
+                (err, csv) => {
+                    if (err) return res.status(500).send(err);
+                    const response = {
+                        message: "Succefully updated ",
+                        updatedtedCommon: csv
+                    };
+                    console.log("Alter on C300CSV ID:", req.params.id, "by:", authData.username);
+                    return res.status(200).send(response);
+                });
         }
-    )
+
+    });
 });
+
+
+
 
 //delete
 
-router.delete('/common/:id', async(req,res)=>{
+router.delete('/common/:id',verifyToken, async(req,res)=> {
+    jwt.verify(req.token, 'secret', (err, authData) => {
+        if (err) {
+            res.status(403).json('Authorization not found');
+            console.log('Authorization not found');
+        } else {
 
-    Csv.findByIdAndRemove(req.params.id, (err,csv)=> {
+            Csv.findByIdAndRemove(req.params.id, (err, csv) => {
 
 
-        if (err) return res.status(500).send(err);
-        const response = {
-            message: "Register succefully deleted",
-            id: Csv.id
-        };
-        return res.status(200).send(response);
+                if (err) return res.status(500).send(err);
+                const response = {
+                    message: "Register succefully deleted",
+                    id: csv.id
+                };
+                console.log("Register ", csv.id,"deleted by: ", authData.username);
+                return res.status(200).send(response);
+
+            });
+        }
+
     });
-
 });
-
-//select all
-
-router.get('/common', async (req,res)=> {
-
-        Csv.find({},function (err, doc) {
-            if (doc) {
-                res.status(200).json(doc);
-            } else {
-                res.status(404).send(err)
-            }
-
-
-        })
-            .catch(err=>{
-                return res.status(500).json({error:err});
-            })
-    }
-);
 
 
 //select by id
-router.get('/common/:id', async (req,res)=> {
+    router.get('/common/:id', verifyToken, async (req, res) => {
 
-    Csv.findById(req.params.id,function (err, doc) {
+        jwt.verify(req.token, 'secret', (err, authData) => {
+            if (err) {
+                res.status(403).json('Authorization not found');
+                console.log('Authorization not found');
+            } else {
 
-        if (doc) {
-            res.status(200).json(doc);
-        } else {
-            res.status(404).json({
-                message: 'ID not valid'
-            })
+                Csv.findById(req.params.id, function (err, doc) {
+
+                    if (doc) {
+                        res.status(200).json(doc);
+                    } else {
+                        res.status(404).json({
+                            message: 'ID not valid'
+                        })
+                    }
+
+
+                })
+                    .catch(err => {
+                        return res.status(500).json({error: err});
+                    })
+            }
+
+        });
+
+    });
+
+
+//select all
+
+    router.get('/common', verifyToken, (req, res) => {
+
+
+        jwt.verify(req.token, 'secret', (err, authData) => {
+
+
+            if (err) {
+                res.status(403).json('Authorization not found');
+                console.log('Authorization not found');
+            } else {
+
+                Csv.find({}, function (err, doc) {
+                    if (doc) {
+                        res.status(200).send(doc);
+                    } else {
+                        res.status(404).send(err)
+                    }
+
+
+                })
+                    .catch(err => {
+                        return res.status(500).json({error: err});
+                    })
+
+            }
+
+        });
+    });
+
+
+    function verifyToken(req, res, next) {
+
+
+        const bearerHeader = req.headers['authorization'];
+        if (!bearerHeader) {
+            res.status(403).send('Auth required');
+        }
+        if (typeof bearerHeader !== 'undefined') {
+            const bearer = bearerHeader.split(' ');
+            const token = bearer[1];
+            req.token = token;
+
+            next();
         }
 
-
-    })
-        .catch(err=>{
-            return res.status(500).json({error:err});
-        })
-
-
-
-
-});
-
-
-
+    }
 
 
 
