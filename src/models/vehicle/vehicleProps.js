a792df7499c928bba45d89ce9f1d9e5c777c97c9
breno <breno.beierstedt@hotmail.com>
2019-01-20 07:34:41 -0200

    vehicle props added

---
+++ src/models/vehicle/vehicleProps.js
@@ -0,0 +1,86 @@
+const mongoose = require('../../dataBase');
+autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(mongoose);
+
+
+
+
+
+const VhpSchema = new mongoose.Schema({
+    "referencia":{
+        type:String,
+
+    },
+    "fipe_codigo":{
+        type:String,
+
+    },
+    "name":{
+        type: String,
+
+
+
+    },
+    "combustivel":{
+        type: Number,
+
+
+
+    },
+    "marca":{
+        type: String,
+
+
+
+    },
+    "ano_modelo":{
+        type: Number,
+
+
+
+    },
+    "preco":{
+        type: Number,
+
+
+
+    },
+    "key":{
+        type: String,
+
+
+
+    },
+    "time":{
+        type: Number,
+
+
+
+    },
+    "veiculo":{
+        type: String,
+
+
+
+    },
+    "id":{
+        type: Number,
+
+
+
+    },
+
+    "createdAt":{
+        type : Date,
+        default: Date.now,
+    },
+},{collection: 'C550VHP'});
+
+
+
+
+VhpSchema.plugin(autoIncrement.plugin, 'MnfCod');
+var VhpCod = mongoose.model('MnfCod', VhpSchema);
+const Vhp = mongoose.model('c552vhp', VhpSchema);
+
+module.exports = Vhp;
