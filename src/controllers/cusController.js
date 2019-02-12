5b5da4cc5e50efa4d40f43d6a5a4c87dee810b13
Breno <breno.beierdtedt@hotmail.com>
2019-02-11 23:12:15 -0200

    changed token parameters

--- src/controllers/cusController.js
+++ src/controllers/cusController.js
@@ -4,7 +4,7 @@ const Cus = require('../models/customer');
 
 const jwt = require('jsonwebtoken');
 
-const router = express.Router();
+const router = express();
 
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -106,7 +106,7 @@ router.delete('/customer/:id',verifyToken, async(req,res)=> {
 
 
 //select by id
-router.get('/customer/:id', verifyToken, async (req, res) => {
+/*router.get('/customer/:id', verifyToken, async (req, res) => {
 
     jwt.verify(req.token, 'secret', (err, authData) => {
         if (err) {
@@ -135,9 +135,9 @@ router.get('/customer/:id', verifyToken, async (req, res) => {
 
 });
 
-
+*/
 //select all
-/*
+
 router.get('/customer', verifyToken, (req, res) => {
 
 
@@ -149,7 +149,7 @@ router.get('/customer', verifyToken, (req, res) => {
             console.log('Authorization not found');
         } else {
 
-            CUs.find({}, function (err, doc) {
+            Cus.find({}, function (err, doc) {
                 if (doc) {
                     res.status(200).send(doc);
                 } else {
@@ -166,34 +166,9 @@ router.get('/customer', verifyToken, (req, res) => {
 
     });
 });
-*/
-
-router.get('/customer',  (req, res) => {
 
 
 
-
-
-var query = Cus.find();
-
-query.select('CusNam Cf1Num CusCod CusEma -_id');
-
-query.exec(function (err, doc) {
-    if (err) return handleError(err);
-    if(!doc){
-        res.status(404).send(err);
-    }else {
-
-                    res.status(200).send(doc);
-                }
-
-
-            })
-
-
-
-
-});
 
 
 function verifyToken(req, res, next) {
@@ -204,14 +179,44 @@ function verifyToken(req, res, next) {
         res.status(403).send('Auth required');
     }
     if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
-        const token = bearer[1];
+
+        const bearer = bearerHeader.split(':');
+
+        const split = bearer[1];
+        const regex = split.replace(/}/gi, "");
+        const token = regex.replace(/"/gi, "");
+
         req.token = token;
 
         next();
     }
 
 }
+
+router.get('/customer/:id',  async (req, res) => {
+
+
+
+            Cus.findById(req.params.id, function (err, doc) {
+
+                if (doc) {
+                    res.status(200).json(doc);
+                } else {
+                    res.status(404).json({
+                        message: 'ID not valid'
+                    })
+                }
+
+
+            })
+                .catch(err => {
+                    return res.status(500).json({error: err});
+                })
+
+
+
+
+});
 
 
 
